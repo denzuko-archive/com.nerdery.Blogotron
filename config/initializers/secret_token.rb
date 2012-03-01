661f6f1f1b361b6ae51b7a0b9be51f550c77c723
Jordan T. Cox <jordan.t.cox@gmail.com>
2012-02-29 19:10:50 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ComNerderyBlogotron::Application.config.secret_token = 'db0d475add91103cf1c800383da7fbca024f903c20fabb9c065e7426be10fcaa1428c9e0de98c9b15dfc2013215c1ef1c4a94642eafca60856f44e72cb268093'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ComNerderyBlogotron::Application.config.session_store :cookie_store, key: '_com.nerdery.Blogotron_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ComNerderyBlogotron::Application.config.session_store :active_record_store
